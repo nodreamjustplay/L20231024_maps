250657f0e80acc6c7e08a7139e27b33a162d39c6
nodreamjustplay <ihjeong95@naver.com>
2023-10-24 17:09:25 +0900

    if문 추가

--- main.cpp
+++ main.cpp
@@ -1,14 +1,60 @@
 #include <iostream>
-
+#include <conio.h>
+#include <windows.h>
 
 using namespace std;
 
 int main()
 {
+	int PlayerX = 1;
+	int PlayerY = 1;
+	char PlayerShape = 'P';
+	bool bIsRunning = true;
 
-	cout << "second push" << endl;
 
+	while (bIsRunning)
+	{
+		int Key = _getch();
+
+		system("cls");
+
+		if (Key == 'a' || Key == 'A')
+		{
+			cout << "왼쪽" << endl;
+			PlayerX--;
+		}
+		else if (Key == 'w' || Key == 'W')
+		{
+			cout << "위" << endl;
+			PlayerY--;
+		}
+		else if (Key == 's' || Key == 'S')
+		{
+			cout << "아래" << endl;
+			PlayerY++;
+		}
+		else if (Key == 'd' || Key == 'D')
+		{
+			cout << "오른쪽" << endl;
+			PlayerX++;
+		}
+		else if (Key == 'q' || Key == 'Q')
+		{
+			cout << "종료" << endl;
+			bIsRunning = false;
+		}
+
+		cout << PlayerX << ", " << PlayerY << endl;
+
+		//x, y 좌표 설정
+		COORD pos = { PlayerX, PlayerY };
+		//커서 이동
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+		cout << PlayerShape;
+	}
 
 	return 0;
+
+
 
 }
